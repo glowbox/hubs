d2f7d844e9558db98d433a05a4b8e869db98ff18
Thomas Wester <thomas@thomaswester.com>
2021-05-14 13:28:12 -0700

    bump webrtp-adapter version

--- webpack.config.js
+++ webpack.config.js
@@ -201,7 +201,7 @@ async function fetchAppConfigAndEnvironmentVars() {
   process.env.SHORTLINK_DOMAIN = shortlink_domain;
   process.env.CORS_PROXY_SERVER = `${localIp}:8080/cors-proxy`;
   process.env.THUMBNAIL_SERVER = thumbnail_server;
-  process.env.NON_CORS_PROXY_DOMAINS = `${localIp},hubs.local,localhost`;
+  process.env.NON_CORS_PROXY_DOMAINS = `${localIp},hubs.local,localhost,mux.com,volumetricperformance.art`;
 
   return appConfig;
 }
